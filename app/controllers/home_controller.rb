c460d79f2f3e0610b7e900c57b46d23dbf4d9116
Muhammed Ali SÜRÜCÜ <progmas@gmail.com>
2019-10-30 19:27:49 +0300

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+
+    @months = ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık']
+    @days = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar']
+  end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,25 @@
+module ApplicationHelper
+  def generate_date(quantity)
+    generated_dates = []
+    quantity.times do
+      generated_dates << rand(Date.new(1900, 1, 1)..Date.new(2100, 12, 31))
+    end
+
+    result_days = Array.new(7,0)
+    generated_dates.each do |day|
+      result_days[day.wday] = result_days[day.wday].to_i + 1
+    end
+
+    return result_days
+  end
+
+  def calc_conflict(dates)
+    count = 0
+    dates.each do |date_count|
+      if date_count > 1
+        count += date_count - 1
+      end
+    end
+    return count
+  end
+end
